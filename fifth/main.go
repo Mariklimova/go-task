9e51513764a327b04a885546209254f4ee98f104
Mariklimova <mariku22@yandex.ru>
2025-04-26 21:50:18 +0300

    added tasks 8-9 for fifth

--- fifth/main.go
+++ fifth/main.go
@@ -115,6 +115,49 @@ func incl_el(sl []int, num int) bool {
 	return flag
 }
 
+// 8. Напишите функцию, которая принимает два числа и операцию (например, +, -, *, /) и выполняет
+// соответствующую арифметическую операцию.
+func calculate(a float64, b float64, operation string) (float64, error) {
+	switch operation {
+	case "+":
+		return a + b, nil
+	case "-":
+		return a - b, nil
+	case "*":
+		return a * b, nil
+	case "/":
+		if b == 0 {
+			return 0, fmt.Errorf("деление на ноль")
+		}
+		return a / b, nil
+	default:
+		return 0, fmt.Errorf("неизвестная операция: %s", operation)
+	}
+}
+
+// 9. Напишите функцию, которая принимает слайс целых чисел и возвращает среднее
+// арифметическое этих чисел.
+
+func average_value(sl []int) int {
+	var sum int
+
+	for _, item := range sl {
+		sum += item
+	}
+	return sum/len(sl)
+}
+
+// 10. Создайте функцию, которая принимает массив чисел, находит все повторяющиеся элементы и
+// удаляет их. После этого функция должна проверить, не является ли массив пустым, и вывести
+// соответствующее сообщение.
+
+// 11. Разработайте программу, которая находит сумму всех нечётных чисел в слайсе и выводит их
+// индексы.
+
+// 12. Напишите программу, которая проверяет, является ли слайс чисел палиндромом, то есть
+// читается ли слайс одинаково в обоих направлениях. В случае палиндрома программа должна
+// вывести true, иначе false.
+
 func main() {
 	fmt.Println(verific(5))
 	fmt.Println(sum_el([]int{1, 2, 3, 4, 5}))
@@ -123,4 +166,6 @@ func main() {
 	fmt.Println(divide_el([]int{1, 2, 3, 4, 5}))
 	fmt.Println(without_ou([]int{1, 0, 2, 3, 0, 4, 5}))
 	fmt.Println(incl_el([]int{1, 0, 2, 3, 0, 4, 5}, 7))
+	fmt.Println(calculate(10, 5, "+"))
+	fmt.Println(average_value([]int{1, 2, 3, 4, 5}))
 }
